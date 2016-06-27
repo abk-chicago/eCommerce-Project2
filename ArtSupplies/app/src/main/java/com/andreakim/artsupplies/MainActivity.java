4ba4e3970ce7b6f04efaa27b3ac64725e0005d8d
Andrea Kim <andreaforanimals@gmail.com>
2016-06-27 16:46:05 -0500

    baby step #1...

--- ArtSupplies/app/src/main/java/com/andreakim/artsupplies/MainActivity.java
+++ ArtSupplies/app/src/main/java/com/andreakim/artsupplies/MainActivity.java
@@ -2,6 +2,9 @@ package com.andreakim.artsupplies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load("http://www.siskefilmcenter.org/wp-content/uploads/2016/03/401.jpg")
+                .into(imageView);
+
+
+
     }
 }
+
